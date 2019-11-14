c5cb7b6b428e5940b0ff8f1af0abd5136de36b6e
UmarEmp <turdubaev.emp@gmail.com>
2019-11-14 10:42:32 +0600

    LoadProject
    LoadProject

---
+++ Assets/Scripts/JsonScripts.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+public class JsonScripts : MonoBehaviour
+{
+    public Transform GameManager;
+    public string  json;
+    public int id;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+  /*  void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            CollectInfo();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadInfo();
+        }
+    }*/
+    [Serializable]
+    public class MusicalLevel {
+        public Vector3 vecPosition;
+    }
+
+
+
+
+    public void CollectInfo()
+    {
+        MusicalLevel[] musicalLevel = new MusicalLevel[100];
+        for (int i = 0; i < GameManager.childCount; i++)
+        {
+            musicalLevel[i].vecPosition = transform.position;      
+        }
+        json = JsonHelper.ToJson(musicalLevel);
+        File.WriteAllText("C:/save.txt", json);
+        Debug.Log(json);
+    }
+   /* public void LoadInfo()
+    {
+        
+        var json1 = File.ReadAllText("C:/save.txt");
+        JsonUtility.FromJson(json, MusicalLevel[0]);
+        for (int i = 0; i < GameManager.childCount; i++)
+        {
+            GameManager.transform.GetChild(i).transform.position = musicalLevel[i].vecPosition;
+        }
+        Debug.Log(json);
+    }*/
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    }
+    
+}
+
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+using System.Linq;
+
+
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform sphereObject;
+    public Material red, green, blue;
+    AudioSource audioSource;
+    AudioClip audioClip;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+        audioSource = GetComponent<AudioSource>();
+        Debug.Log("Audio clip length : " + audioSource.clip.length);
+        MidiFile midiFile = MidiFile.Read("HouseDemo.mid");
+     
+      //  midiFile.ReplaceTempoMap(TempoMap.Create(Tempo.FromBeatsPerMinute(727)));
+        TempoMap tempoMap = midiFile.GetTempoMap();
+        var midiFileDuration = midiFile.GetTimedEvents()
+                              .LastOrDefault (e => e.Event is NoteOffEvent)
+                             ?.TimeAs<MetricTimeSpan>(tempoMap)
+                             ?? new MetricTimeSpan();
+        
+        
+        Debug.Log(midiFileDuration);
+ 
+        float TrackMidiLengths = (midiFileDuration.TotalMicroseconds) / (audioSource.clip.length*1000000f);
+        //midiFile.ProcessTimedEvents(e => e.Time /= Mathf.FloatToHalf(TrackMidiLengths));
+        IEnumerable<Note> notes = midiFile.GetNotes();
+        float[] arrayTimeForSecond = new float[notes.ToArray().Length];
+
+        Debug.Log(TrackMidiLengths);
+        for (int i = 0; i < notes.ToArray().Length; i++)
+        {
+
+            if (i==0) {
+                MetricTimeSpan metric = notes.ToArray()[i].TimeAs<MetricTimeSpan>(tempoMap); float a = metric.TotalMicroseconds / 1000000f;
+                Debug.Log("a=="+a);
+                MetricTimeSpan metricl = notes.ToArray()[i].LengthAs<MetricTimeSpan>(tempoMap); float al = metricl.TotalMicroseconds / 1000000f;
+                Debug.Log("a==" + al);
+
+            }
+            MetricTimeSpan metricLength = notes.ToArray()[i].LengthAs<MetricTimeSpan>(tempoMap);
+            MetricTimeSpan metricTime = notes.ToArray()[i].TimeAs<MetricTimeSpan>(tempoMap);
+        
+            arrayTimeForSecond[i] = metricTime.TotalMicroseconds/1000000f / TrackMidiLengths/1.039f+metricLength.TotalMicroseconds/2000000f;
+           // Debug.Log(arrayTimeForSecond[i]);
+
+
+        }
+
+
+        for (int i=0; i < arrayTimeForSecond.Length; i++)
+        {
+           // float r1 = Random.Range(-0.5f, 0.5f);
+           // float r2 = Random.Range(-0.5f, 0.5f);
+            var newObj = Instantiate(sphereObject, new Vector3(0,0, arrayTimeForSecond[i] * 2f), Quaternion.Euler(0, 0, 0), transform);
+            if(i % 3 == 0) newObj.transform.GetComponent<Renderer>().sharedMaterial = red;
+            else if(i % 3 == 1) newObj.transform.GetComponent<Renderer>().sharedMaterial = green;
+            else newObj.transform.GetComponent<Renderer>().sharedMaterial = blue;
+
+           // Debug.Log(arrayTimeForSecond[i]);
+        }
+
+        // Debug.Log(tempoMap.Tempo.ToString());
+    }
+
+    void oylesine()
+    {
+        /* for (int i = 0; i < arrayTimeForSecond.Length-1; i++)
+ {
+     float k = arrayTimeForSecond[i]; int ii = i+1; ;  bool prk = true;
+     while (prk)
+     {
+         if (k == arrayTimeForSecond[ii] && ii< arrayTimeForSecond.Length-1)
+         {
+             ii++;
+         }
+         else prk = false;
+     }
+     if (ii > i + 1  )
+     {
+         float pogr = 0.05f;
+         for(int j=i+1; j < ii; j++)
+         {
+             arrayTimeForSecond[j] += pogr; pogr += 0.05f;
+         }
+         i = ii + 1;
+     }
+     //Debug.Log(arrayTimeForSecond[i]);
+
+ }*/
+
+        //   Debug.Log(notes.ToArray().Length);
+    }
+}
