6a1c3c16ae5e40de68f278568da0d599d8844bb6
UmarEmp <turdubaev.emp@gmail.com>
2019-11-28 22:02:24 +0600

    Revert "WithController"
    This reverts commit ebe8cc44ddd8704877bd1cf918d94a7f7569883b.

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -16,26 +16,25 @@ public class NewBehaviourScript : MonoBehaviour
     AudioClip audioClip;
 
 
-
     // Start is called before the first frame update
     void Awake()
     {
-
+        
         audioSource = GetComponent<AudioSource>();
         Debug.Log("Audio clip length : " + audioSource.clip.length);
         MidiFile midiFile = MidiFile.Read("HouseDemo.mid");
-
-        //  midiFile.ReplaceTempoMap(TempoMap.Create(Tempo.FromBeatsPerMinute(727)));
+     
+      //  midiFile.ReplaceTempoMap(TempoMap.Create(Tempo.FromBeatsPerMinute(727)));
         TempoMap tempoMap = midiFile.GetTempoMap();
         var midiFileDuration = midiFile.GetTimedEvents()
-                              .LastOrDefault(e => e.Event is NoteOffEvent)
+                              .LastOrDefault (e => e.Event is NoteOffEvent)
                              ?.TimeAs<MetricTimeSpan>(tempoMap)
                              ?? new MetricTimeSpan();
-
-
+        
+        
         Debug.Log(midiFileDuration);
-
-        float TrackMidiLengths = (midiFileDuration.TotalMicroseconds) / (audioSource.clip.length * 1000000f);
+ 
+        float TrackMidiLengths = (midiFileDuration.TotalMicroseconds) / (audioSource.clip.length*1000000f);
         //midiFile.ProcessTimedEvents(e => e.Time /= Mathf.FloatToHalf(TrackMidiLengths));
         IEnumerable<Note> notes = midiFile.GetNotes();
         float[] arrayTimeForSecond = new float[notes.ToArray().Length];
@@ -44,42 +43,33 @@ public class NewBehaviourScript : MonoBehaviour
         for (int i = 0; i < notes.ToArray().Length; i++)
         {
 
-            if (i == 0)
-            {
+            if (i==0) {
                 MetricTimeSpan metric = notes.ToArray()[i].TimeAs<MetricTimeSpan>(tempoMap); float a = metric.TotalMicroseconds / 1000000f;
-                Debug.Log("a==" + a);
+                Debug.Log("a=="+a);
                 MetricTimeSpan metricl = notes.ToArray()[i].LengthAs<MetricTimeSpan>(tempoMap); float al = metricl.TotalMicroseconds / 1000000f;
                 Debug.Log("a==" + al);
 
             }
             MetricTimeSpan metricLength = notes.ToArray()[i].LengthAs<MetricTimeSpan>(tempoMap);
             MetricTimeSpan metricTime = notes.ToArray()[i].TimeAs<MetricTimeSpan>(tempoMap);
-
-            arrayTimeForSecond[i] = metricTime.TotalMicroseconds / 1000000f / TrackMidiLengths / 1.039f + metricLength.TotalMicroseconds / 2000000f;
-            // Debug.Log(arrayTimeForSecond[i]);
+        
+            arrayTimeForSecond[i] = metricTime.TotalMicroseconds/1000000f / TrackMidiLengths/1.039f+metricLength.TotalMicroseconds/2000000f;
+           // Debug.Log(arrayTimeForSecond[i]);
 
 
         }
-      /*  List<NotePositin> list = new List<NotePositin>();
-        foreach (float pos in arrayTimeForSecond)
-        {
-            list.Add(new NotePositin(pos));
-        }
-        MyObject myObject = new MyObject(list);
-        myObject.Load();
-        */
 
-        Debug.Log(arrayTimeForSecond.Length);
-        for (int i = 0; i < arrayTimeForSecond.Length; i++)
+
+        for (int i=0; i < arrayTimeForSecond.Length; i++)
         {
-            // float r1 = Random.Range(-0.5f, 0.5f);
-            // float r2 = Random.Range(-0.5f, 0.5f);
-            var newObj = Instantiate(sphereObject, new Vector3(0, 0, arrayTimeForSecond[i] * 2f), Quaternion.Euler(0, 0, 0), transform);
-            if (i % 3 == 0) newObj.transform.GetComponent<Renderer>().sharedMaterial = red;
-            else if (i % 3 == 1) newObj.transform.GetComponent<Renderer>().sharedMaterial = green;
+           // float r1 = Random.Range(-0.5f, 0.5f);
+           // float r2 = Random.Range(-0.5f, 0.5f);
+            var newObj = Instantiate(sphereObject, new Vector3(0,0, arrayTimeForSecond[i] * 2f), Quaternion.Euler(0, 0, 0), transform);
+            if(i % 3 == 0) newObj.transform.GetComponent<Renderer>().sharedMaterial = red;
+            else if(i % 3 == 1) newObj.transform.GetComponent<Renderer>().sharedMaterial = green;
             else newObj.transform.GetComponent<Renderer>().sharedMaterial = blue;
 
-            // Debug.Log(arrayTimeForSecond[i]);
+           // Debug.Log(arrayTimeForSecond[i]);
         }
 
         // Debug.Log(tempoMap.Tempo.ToString());
---
+++ Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    public Transform GameManager;
+    public List<MyObject> objects = new List<MyObject>(500);
+    List<MyNestedObject> nestedObjects = new List<MyNestedObject>();
+
+
+    public string json;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            CollectInfo();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadInfo();
+        }
+    }
+
+    public void CollectInfo()
+    {
+        MyWrapper myWrapper = new MyWrapper(objects);
+
+        for (int i = 0; i < objects.Count; i++) objects.Add(null);
+        for (int i = 0; i < GameManager.childCount; i++)
+        {   
+            
+            myWrapper.objects[0].nestedObjects[i].vecPosition = GameManager.transform.GetChild(i).transform.position;
+        }
+        json = JsonUtility.ToJson(myWrapper);
+        File.WriteAllText("C:/save.txt", json);
+        Debug.Log(json);
+    }
+    public void LoadInfo()
+    {
+        
+    MyWrapper myWrapper = new MyWrapper(objects);
+        json = File.ReadAllText("C:/save.txt");
+        JsonUtility.FromJson<MyWrapper>(json);
+        for (int i = 0; i < GameManager.childCount; i++)
+        {
+            GameManager.transform.GetChild(i).transform.position = myWrapper.objects[0].nestedObjects[i].vecPosition;
+        }
+        Debug.Log(json);
+
+
+    }
+}
+[Serializable]
+public class MyWrapper
+{
+    [SerializeField]
+    public List<MyObject> objects;
+    public MyWrapper(List<MyObject> objects) {
+        this.objects = objects;
+    }
+
+}
+[Serializable]
+public class MyObject
+{
+    [SerializeField]
+    public List<MyNestedObject> nestedObjects;
+    public MyObject(List<MyNestedObject> nestedObjects)
+    {
+        this.nestedObjects = nestedObjects;
+    }
+}
+[Serializable]
+public class MyNestedObject
+{
+    [SerializeField]
+    public Vector3 vecPosition;
+}
+
+
+
